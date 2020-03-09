b873a68c975bbe67c4d70e64de02663ead8a9dd1
chenyangyao <652415049@qq.com>
2020-03-10 05:00:50 +0800

    2020-03-10 05:00:49

---
+++ labs/week-7/wdir/open_crit/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for i in 0 1 2 3 4 5 6 7 8 9
+do
+    for j in 0 1 2 3 4 5 6 7 8 9
+    do
+        wd="$PWD/runs_alpha${i}.${j}"
+        name="runs${i}.${j}"
+        cmd="mpirun -np 8 Gadget2 param.txt"
+        _cmd="msub -j mpi -p 8 -d ${wd} -wd ${wd} -n ${name} -m 16gb ${cmd}"
+        echo ${_cmd}
+    done
+done
