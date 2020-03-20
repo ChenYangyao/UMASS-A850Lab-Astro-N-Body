8abbbe81de9e674738435debced1e68e6c9a01ad
chenyangyao <652415049@qq.com>
2020-03-21 03:36:23 +0800

    2020-03-21 Sat 03:36:23

--- labs/week-8/wdir/chg_Npart/run.sh
+++ labs/week-8/wdir/chg_Npart/run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-for i in  1000 5000 10000 50000 100000 500000 1000000
+for i in  5000 10000 50000 100000 500000 1000000
 do
     wd="$PWD/n$i"
     name="n$i"
 
-    _cmd="msub -j mpi -p 16 -n ${name} -d ${wd} -wd ${wd} -m 32gb"
-    ${_cmd} 'date "+%T"' 'mpirun -np 16 Gadget2 param.txt' 'date "+%T"'
+    _cmd="msub -j mpi -c 16 -p 4 -mpiprocs 4 -m 16gb -n ${name} -d ${wd} -wd ${wd}"
+    ${_cmd} 'date "+%T"' 'mpirun -np 64 Gadget2 param.txt' 'date "+%T"'
 done
