a9c152ee4557f6f9d055e23dcaea29e9de778a49
chenyangyao <652415049@qq.com>
2020-03-21 14:54:13 +0800

    2020-03-21 Sat 14:54:13

---
+++ labs/week-8/chg_openAng/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+alphas=(0.0010 0.0020 0.0050 0.0100)
+
+for i in  {0..3}
+do
+    alpha=${alphas[i]}
+
+    wd="$PWD/alpha$alpha"
+    name="a$alpha"
+
+    _cmd="msub -j mpi -c 1 -p 16 -mpiprocs 16 -m 24gb -n ${name} -d ${wd} -wd ${wd}"
+    ${_cmd} 'date "+%T"' "mpirun -np 16 Gadget2 param.txt" 'date "+%T"'
+done
