3dcf83572544c6e2bdb17218d44ef8932acadeba
chenyangyao <652415049@qq.com>
2020-03-22 06:48:53 +0800

    2020-03-22 Sun 06:48:53

--- labs/week-8/wdir/chg_dt/run.sh
+++ labs/week-8/wdir/chg_dt/run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-alphas=(0.1000 0.2000 0.5000 1.0000 2.0000 5.0000 10.0000 20.0000 50.0000 100.0000)
+dts=(0.1000 0.2000 0.5000 1.0000 2.0000 5.0000 10.0000 20.0000 50.0000 100.0000)
 
 for i in  {0..9}
 do
-    alpha=${alphas[i]}
+    dt=${dts[i]}
 
-    wd="$PWD/alpha$alpha"
-    name="alpha$alpha"
+    wd="$PWD/dt$dt"
+    name="dt$dt"
 
     _cmd="msub -j mpi -c 1 -p 16 -mpiprocs 16 -m 24gb -n ${name} -d ${wd} -wd ${wd}"
     ${_cmd} 'date "+%T"' "mpirun -np 16 Gadget2 param.txt" 'date "+%T"'
--- labs/week-8/wdir/chg_soft/run.sh
+++ labs/week-8/wdir/chg_soft/run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-alphas=(0.1000 0.2000 0.5000 1.0000 2.0000 5.0000 10.0000 20.0000 50.0000 100.0000)
+softs=(0.0500 0.1000 0.2000 0.5000 1.0000 2.0000 5.0000 10.0000 50.0000 100.0000)
 
 for i in  {0..9}
 do
-    alpha=${alphas[i]}
+    soft=${softs[i]}
 
-    wd="$PWD/alpha$alpha"
-    name="alpha$alpha"
+    wd="$PWD/soft$soft"
+    name="soft$soft"
 
     _cmd="msub -j mpi -c 1 -p 16 -mpiprocs 16 -m 24gb -n ${name} -d ${wd} -wd ${wd}"
     ${_cmd} 'date "+%T"' "mpirun -np 16 Gadget2 param.txt" 'date "+%T"'
