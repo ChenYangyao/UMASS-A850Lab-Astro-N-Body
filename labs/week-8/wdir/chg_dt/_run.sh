6eb17246342e891b3eb00e450bfc2eb4815c1cf2
chenyangyao <652415049@qq.com>
2020-03-23 00:48:20 +0800

    2020-03-23 Mon 00:48:20

--- labs/week-8/wdir/chg_dt/_run.sh
+++ labs/week-8/wdir/chg_dt/_run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-dts=(0.1000 0.2000 0.5000 1.0000 2.0000 5.0000)
+dts=(10.0000 20.0000 50.0000 100.0000)
 
-for i in  {0..5}
+for i in  {0..3}
 do
     dt=${dts[i]}
 
     wd="$PWD/dt$dt"
     name="dt$dt"
 
-    _cmd="msub -j mpi -c 4 -p 8 -mpiprocs 8 -m 16gb -n ${name} -d ${wd} -wd ${wd}"
+    _cmd="msub -j mpi -c 5 -p 24 -mpiprocs 24 -m 48gb -n ${name} -d ${wd} -wd ${wd}"
     ${_cmd} 'date "+%T"' "mpirun -np 32 Gadget2 param.txt" 'date "+%T"'
 done
