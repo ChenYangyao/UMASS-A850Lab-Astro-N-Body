00cad01dde05e8ad1469c3f89ce6c44c40417759
chenyangyao <652415049@qq.com>
2020-03-31 11:46:16 +0800

    2020-03-31 Tue 11:46:15

--- lib/nbd_util/coord.py
+++ lib/nbd_util/coord.py
@@ -8,3 +8,33 @@ class CoordsCvt:
     def sphere_to_cart(theta, phi):
         return np.array([np.sin(theta)*np.cos(phi), 
             np.sin(theta)*np.sin(phi), np.cos(theta)])
+
+    @staticmethod
+    def normalize(v):
+        return v / np.linalg.norm(v, axis=1).reshape((len(v),-1))
+
+    @staticmethod
+    def rt_to_cart(x, v):
+        ''' radial-tangential coordinate to cartesian
+        Parameters
+        ----------
+        x: (n,3) array-like
+            spatial position
+        v: (n,3) array-like
+            vector to be transformed
+        Return (n,3) array-like, the cartesian coordinate
+        '''
+        vnew_z = np.array(x)
+        vold_z = np.array([0,0,1])
+        vnew_x = np.cross( vold_z, vnew_z )
+        vnew_y = np.cross( vnew_z, vnew_x )
+
+        vnew_x = CoordsCvt.normalize(vnew_x)
+        vnew_y = CoordsCvt.normalize(vnew_y)
+        vnew_z = CoordsCvt.normalize(vnew_z)
+
+        v = np.array(v)
+        return vnew_x*v[:,[0]]+vnew_y*v[:,[1]]+vnew_z*v[:,[2]]
+
+
+
--- lib/nbd_util/random.py
+++ lib/nbd_util/random.py
@@ -7,10 +7,15 @@ class RNG:
     
     @staticmethod
     def sphere( n ):
-        cos_t = 2*RNG.uniform(n, 1.0e-6, 1.0-1.0e-6)-1.0
+        cos_t = 2*RNG.uniform(n, 1.0e-10, 1.0-1.0e-10)-1.0
         t = np.arccos( cos_t )
         phi = 2.0*np.pi*RNG.uniform(n)
         return CoordsCvt.sphere_to_cart( t, phi ).T
+
+    @staticmethod
+    def circle( n ):
+        t = RNG.uniform(n, 0., 2.0*np.pi)
+        return np.array([np.cos(t), np.sin(t)]).T
 
     @staticmethod
     def uniform( n, lo=0., hi=1. ):
