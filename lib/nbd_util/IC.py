713112a12883e1e93f2daa30475ad7acc5069228
chenyangyao <652415049@qq.com>
2020-04-20 18:18:52 +0800

    2020-04-20 Mon 18:18:49

--- lib/nbd_util/IC.py
+++ lib/nbd_util/IC.py
@@ -1,75 +1,125 @@
 from .random import RNG
+from .coord import CoordsCvt
+from .units import Units
 import numpy as np
 import scipy.optimize as opt
+import scipy.interpolate as interp
 
 class Jeans:
     def __init__(self, profile):
-        self.profile = profile
-        self.r0, self.rt = profile.r0, profile.rt
-        self.mass = profile.mass
+        pf = profile
+        self._tags = { 'profile': pf, 
+            'mass': pf.M( pf['rvir'] ),
+            'm_hi': 1., 'm_lo': 0., 
+            'r0': pf['r0'], 'rt': pf['rt'], 'rw': pf['rw'] }
+    def __getitem__(self, key):
+        return self._tags[key]
     def get(self, n):
-        M = RNG.uniform( n, 1.0e-4, 0.9999 ) * self.mass
-        r = self.profile.r_at_M( M )
-        sigma = np.sqrt(self.profile.sigma2( r ))
+        pf = self['profile']
+        M = RNG.uniform( n, self['m_lo'], self['m_hi'] ) * self['mass']
+        r = pf.r_at_M( M )
+        sigma = np.sqrt(pf.sigma2( r ))
         
         pos = RNG.sphere(n) * r.reshape((n,1))
         vel = np.random.randn(n, 3) * sigma.reshape((n,1))
         vel = vel - vel.mean( axis=0 )
-        partmass = self.mass / n
-        print('range: pos=', np.max(np.abs(pos)), ', vel=', np.max(np.abs(vel)), 
-              ', particle mass=', partmass)
+        partmass = self['mass'] / n
+        print('range: pos=%g, vel=%g, m_part=%g, sampl. rate=%g'%(\
+            np.max(np.abs(pos)), np.max(np.abs(vel)), partmass, 1.0))
         return pos, vel, partmass
 
-
 class EddInv:
-    def __init__(self, prof):
-        self.prof = prof
-        self.mass = prof.mass
-        self.r0, self.rt = prof.r0, prof.rt
-        self.Et = prof.V( prof.rt )
-        self.f_optimize_target = lambda x,y: 1.5*x - 0.5*x*x*x - y
+    def __init__(self, profile):
+        pf = profile
+        
+        Et = pf.V( pf['rt'] )
+        
+        f_opt = lambda x,y: 0.25*(3.0*x - x*x*x) + 0.5 - y
+        rands = np.linspace(0, 1, 10000)
+        xs = [ opt.root_scalar( 
+            f_opt, args=(rand,), bracket=[-1,1], method='brentq' ).root \
+              for rand in rands ]
+        r2vr = interp.PchipInterpolator(rands, xs)
+        
+        self._tags = { 'profile': pf, 
+            'mass': pf.M( pf['rvir'] ),
+            'r2vr': r2vr, 'Et': Et,
+            'm_hi': 1., 'm_lo': 0., 
+            'r0': pf['r0'], 'rt': pf['rt'], 'rw': pf['rw'] }
+    def __getitem__(self, key):
+        return self._tags[key]
     def vmax(self, V):
-        return np.sqrt( 2.0*(self.Et - V) )
-    def one_sample(self):
-        R0, R1, R2, R3 = RNG.uniform( 4, 1.0e-4, 0.9999 )
-        M = R0 * self.mass
-        r = self.prof.r_at_M( M )
-        V = self.prof.V(r)
+        dV = self['Et'] - V
+        if np.any(dV < -1.0e-10):
+            raise Exception('Error V %.6g > Et %.6g'%(V[dV < -1.0e-10], 
+                self['Et']))
+        dV[ dV < 0. ] = 0.
+        return np.sqrt( 2.0*dV )
+    def samples(self, n):
+        pf = self['profile']
+        M = RNG.uniform( n, self['m_lo'], self['m_hi'] ) * self['mass']
+        r = pf.r_at_M(M)
+        V = pf.V(r)
         vmax = self.vmax(V)
-        
-        z = np.sqrt(R1)
-        vr = opt.root_scalar( self.f_optimize_target, 
-            args=(R2,), bracket=[0., 1.], method='brentq' ).root * vmax
-        vr2 = vr*vr
         vmax2 = vmax*vmax
-        vt2 = z*z*( vmax2-vr2 )        
-        fs = self.prof.fE( np.array([ (vr2+vt2)/2.0+V, V ]) )
-        reject_thres = fs[0] / (fs[1]+1.0e-15)
-        
-        if R3 > reject_thres: return None
-        vt = np.sqrt(vt2)
-        return r, vr, vt
-    def samples( self, n ):
-        s = []
-        cnt = 0
-        while len(s) < n:
-            sample = self.one_sample()
-            cnt += 1
-            if sample: s.append(sample)
-        print('sampling rate = ', n/cnt)
-        r, vr, vt = np.array(s).T
+        fV = pf.fE(V)
+        vr, vt = np.zeros(n), np.zeros(n)
+        vr[ fV == 0. ] = 0.
+        vt[ fV == 0. ] = 0.
+        ids_todo = np.arange(n,dtype=int)[ fV != 0. ]
+        n_trys = 0
+        while len(ids_todo) > 0:
+            n_ids = len(ids_todo)
+            n_trys += n_ids
+            acc, _vr, _vt = self.samples_at_r( r[ids_todo], 
+                V[ids_todo], vmax[ids_todo], vmax2[ids_todo], fV[ids_todo]  )
+            ids_acc = ids_todo[ acc ]
+            vr[ids_acc] = _vr
+            vt[ids_acc] = _vt
+            ids_todo = ids_todo[ acc==False ]
+            if len(ids_todo) < n/5000+5:
+                break
+        print('Begins to chunk implementation for %d samples'%(len(ids_todo)), 
+            end='... ')
+        for id_todo in ids_todo:
+            base_n, fac_n, max_fac_n = 1000, 1, 10000
+            while True:
+                ones = np.ones(base_n*fac_n, dtype=float)
+                n_trys += len(ones)
+                acc, _vr, _vt = self.samples_at_r( r[id_todo]*ones, 
+                    V[id_todo]*ones, vmax[id_todo]*ones, 
+                    vmax2[id_todo]*ones, fV[id_todo]*ones )
+                if len(_vr) > 0:
+                    vr[id_todo] = _vr[0]
+                    vt[id_todo] = _vt[0]
+                    break
+                if fac_n < max_fac_n: fac_n *= 2
+                else: 
+                    print('\n\tFor sample %d find difficulty, r=%g, fV=%g... '%(\
+                        id_todo, r[id_todo], fV[id_todo]), end='')
+                    vr[id_todo] = 0.;
+                    vt[id_todo] = 0.
+                    break
+        return r, vr, vt, n_trys
+    def samples_at_r(self, r, V, vmax, vmax2, fV ):
+        pf = self['profile']
+        n = len(r)
+        R1, R2, R3 = RNG.uniform(3*n, 1.0e-6, 1-1.0e-6).reshape(3,n)
+        z = np.sqrt( R1 )
+        vr = self['r2vr']( R2 ) * vmax
+        vr2 = vr*vr
+        vt2 = z*z*(vmax2-vr2)
+        fE = pf.fE( 0.5*(vr2+vt2)+V )
+        accept = fE / fV > R3
+        return accept, vr[accept], np.sqrt(vt2[accept])
+    def get( self, n ):
+        r, vr, vt, n_trys = self.samples(n)
         pos = RNG.sphere(n) * r.reshape((n,-1))
-        
-        rn = RNG.uniform(n)
-        vr[ rn<.5 ] = -vr[ rn<.5 ]
-        
-        phi = 2.0*np.pi*RNG.uniform(n)
-        vx = np.cos(phi)*vt
-        vy = np.sin(phi)*vt
-        vz = vr
-        vel = np.array([vx,vy,vz]).T
-        vel = vel - vel.mean( axis=0 )
-        partmass = self.mass / n
-        print('range: pos=', np.max(np.abs(pos)), ', vel=', np.max(np.abs(vel)), 
-              ', particle mass=', partmass)
+        circ=RNG.circle(n)*vt.reshape((n,-1))
+        v = np.array([circ[:, 0], circ[:, 1], vr]).T
+        v = CoordsCvt.rt_to_cart( pos, v )
+        vel = v - v.mean( axis=0 )
+        partmass = self['mass'] / n
+        print('finish\n\trange: pos=%g, vel=%g, m_part=%g, sampl. rate=%g'%(\
+            np.max(np.abs(pos)), np.max(np.abs(vel)), partmass, n/n_trys))
         return pos, vel, partmass
--- lib/nbd_util/profile.py
+++ lib/nbd_util/profile.py
@@ -3,162 +3,185 @@ from .units import Units
 import scipy.interpolate as interp
 import scipy.integrate as integ
 import scipy
+import mystats
 import collections
 
-class NFWProfile:
-    def __init__(self, mass, rs, rt=None, rw=None, r0=None, units=None ):
-        self.mass = mass
-        self.rs = rs
-        self.rt = rt if rt else 15.0*rs
-        self.rw = rw if rw else rs
-        self.r0 = r0 if r0 else 1.0e-5 * rs
-        
-        self.xs, self.xt, self.xw, self.x0 = 1.0, \
-            self.rt/self.rs, self.rw/self.rs, self.r0 / self.rs
-        self.ys, self.yt, self.yw, self.y0 = 0., \
-            np.log( self.xt ), np.log( self.xw ), np.log( self.x0 )
-        self.vols = 4.0/3.0*np.pi*rs*rs*rs
-        
-        self.units = units if units else Units()
-        self.G =  self.units['G']
-        
-        #y1, y2 = self.y0, self.yt
-        #ys = np.linspace( y1, y2, 1500 )
-        ys = np.hstack( ( 
-            np.linspace(self.y0, np.log(self.xt-self.xw), 1500 ), 
-            np.log( np.linspace(self.xt-self.xw+0.001*self.xw, self.xt, 100) ) 
-        ) )
-        self._data = {'ys': ys }
-        self.interp = interp.PchipInterpolator
-        
+class HaloProfileBase:
+    def __init__(self):
+        print('%s Profile: Interp begins ...'%(self['name']))
+        self._tags.update({ 
+            'G': self['units']['G'], 'interp': interp.PchipInterpolator,
+            'integ': integ.quad, 'vols': 4.0/3.0*np.pi*self['rs']**3,
+        })
         self._interp_IM()
         self._interp_IV()
         self._interp_Isigma()
         self._interp_If()
     def __getitem__(self, key):
-        return self._data[key]
+        return self._tags[key]
     def rho(self, r):
-        return self.rho0 * self._Irho( r/self.rs )
+        return self['rho0']*self._Irho( r/self['rs'] )
     def M(self, r):
-        return 3.0*self.Ms*self._IM( r/self.rs )
+        return 3.0*self['Ms']*self._IM( r/self['rs'] )
     def V(self, r):
-        return self.V0 + 3.0 * self.Vs * self._IV( r/self.rs )
+        return self['V0'] + 3.0*self['Vs']*self._IV( r/self['rs'] )
     def sigma2(self, r):
-        x = r/self.rs
-        ans = 3.0*self.Vs/self._Irho(x)*self._Isigma(x)
+        x = r/self['rs']
+        ans = 3.0*self['Vs']/self._Irho(x)*self._Isigma(x)
         if isinstance(ans, collections.Iterable):
-            ans[ ans < 0. ] = 0.
+            ans[ans<0.] = 0.
         else:
-            if ans < 0. : ans = 0.
+            if ans<0.: ans = 0.
         return ans
     def r_at_M(self, M):
-        ans = self._inv_IM( M/(3.0*self.Ms) ) * self.rs
-        if isinstance(ans, collections.Iterable):
-            ans[ ans < 0. ] = 0.
-            ans[ ans > self.rt ] = self.rt
-        else:
-            if ans < 0.: ans = 0.
-            if ans > self.rt: ans = self.rt
+        ans = self._inv_IM( M/(3.0*self['Ms']) )*self['rs']
         return ans
     def fE(self, E):
-        xE = np.log(self['IE_eps'] + (E-self.V0)/(3.0*self.Vs))
-        ans = self.fE0 * self._IfxE(xE)
-        if isinstance(ans, collections.Iterable):
-            ans[ans < 0.] = 0.
-        else:
-            if ans < 0.: ans = 0.
+        xE = np.log(self['IE_eps']+(E-self['V0'])/(3.0*self['Vs']))
+        ans = self['fE0'] * self._IfxE(xE)
+        
+        #xE_last = self['xEs'][-400]
+        #if isinstance(xE, collections.Iterable):
+        #    ans[ xE >= xE_last ] = 0.
+        #else:
+        #    if xE >= xE_last: ans = 0.
+        
         return ans
-    def _Irho(self, x):
-        xp1 = 1.0+x
-        return 1.0 / ( x*xp1*xp1 ) * scipy.special.erf( (self.xt-x)/self.xw )
     def _Irhoy(self, y):
         return self._Irho( np.exp(y) )
     def _interp_IM(self):
         Irhoy = self._Irhoy
         def f( y ):
             return np.exp( 3.0*y ) * Irhoy(y)
-        ys = self['ys']
-        IMys = [ integ.quad( f, self.y0, y )[0] for y in ys ]
-        _IMy = self.interp(ys, IMys)
+        y_vals = self['y_vals']
+        IMys = [ self['integ']( f, self['yin'], y )[0] for y in y_vals ]
+        _IMy = self['interp'](y_vals, IMys)
         self._IMy = _IMy
         self._IM = lambda x: _IMy( np.log(x) )
-        self.Ms = self.mass / (3.0*IMys[-1])
-        self.rho0 = self.Ms / self.vols
-        self.Vs = self.G * self.Ms / self.rs
-        print('interp for M done, IM = [%f...%f]'%( IMys[0], IMys[-1] ))
-        _inv_IMy = self.interp( IMys, ys )
-        self._inv_IMy = _inv_IMy
+        Ms = self['mass'] / (3.0*_IMy(self['yvir']))
+        self._tags.update({'Ms': Ms,
+            'rho0':Ms/self['vols'],  'Vs':self['G']*Ms/self['rs']})
+        _inv_IMy = self['interp']( IMys, y_vals )
+        self._inv_IMy = mystats.InterpBound( IMys[0], IMys[-1], _inv_IMy )
         self._inv_IM = lambda IM: np.exp(_inv_IMy(IM) )
-        self._data['IMys'] = np.array(IMys)
-        self._data['Irhoys'] = self._Irhoy( ys )
+        print('  Interp for M done, IM = [%.4g...%.4g]'%( IMys[0], IMys[-1] ))
     def _interp_IV(self):
         IMy = self._IMy
         def f( y ):
             return IMy(y) / np.exp(y)
-        ys = self['ys']
-        IVys = [ integ.quad( f, self.y0, y )[0] for y in ys ]
-        _IVy = self.interp( ys, IVys )
+        y_vals = self['y_vals']
+        IVys = [ self['integ']( f, self['yin'], y )[0] for y in y_vals ]
+        _IVy = self['interp']( y_vals, IVys )
         self._IVy = _IVy
         self._IV = lambda x: _IVy( np.log(x) )
-        self.V0 = -3.0*self.Vs*IVys[-1]
-        print('interp for V done, IV = [%f...%f]'%( IVys[0], IVys[-1] ))
-        self._data['IVys'] = np.array(IVys)
+        self._tags['V0'] = -3.0*self['Vs']*IVys[-1]
+        print('  Interp for V done, IV = [%.4g...%.4g]'%( IVys[0], IVys[-1] ))
     def _interp_Isigma(self):
         IMy, Irhoy = self._IMy, self._Irhoy
         def f(y):
             return IMy(y)*Irhoy(y) / np.exp(y)
-        ys = self['ys']
-        Isigmays = [ integ.quad( f, y, self.yt )[0] for y in ys ]
-        _Isigmay = self.interp(ys, Isigmays)
+        y_vals = self['y_vals']
+        Isigmays = []
+        for i, y in enumerate(y_vals):
+            _int = self['integ']( f, y, self['yt'] )[0]
+            Isigmays.append(_int)
+        _Isigmay = self['interp'](y_vals, Isigmays)
         self._Isigmay = _Isigmay
         self._Isigma = lambda x: _Isigmay( np.log(x) )
-        print('interp for sigma done, Isigma = [%f...%f]'%( Isigmays[0], \
+        print('  Interp for sigma done, Isigma = [%.4g...%.4g]'%( Isigmays[0], \
             Isigmays[-1] ))
-        self._data['Isigmays'] = np.array(Isigmays)
     def _interp_If(self):
-        Irho_eps = IV_eps = IE_eps = Int_eps = q_eps = 0.01
-        self._data.update( dict(Irho_eps=Irho_eps, 
-                IV_eps=IV_eps, IE_eps=IE_eps, Int_eps=Int_eps, q_eps=q_eps) )
+        Irho_eps = 1.0
+        IV_eps = q_eps = IE_eps =  0.01
+        Int_eps = 1.0
+        a_eps = -np.log(q_eps)+q_eps
+        self._tags.update( {'Irho_eps':Irho_eps, 
+            'IV_eps':IV_eps, 'q_eps':q_eps, 'IE_eps':IE_eps, 
+            'Int_eps':Int_eps, 'a_eps':a_eps} )
         
         # make the integrand
         # It consists of a detivative drho / dV, or dx_rho / dx_V
-        Irhos, IVs = self['Irhoys'], self['IVys']
+        Irhos, IVs = self._Irhoy(self['y_vals']), self._IVy(self['y_vals'])
         xrhos, xVs = np.log( Irho_eps + Irhos ), np.log( IV_eps + IVs )
-        _xrhoxV = self.interp( xVs, xrhos )
+        _xrhoxV = self['interp']( xVs, xrhos )
         _dxrhoxV = _xrhoxV.derivative()
-        
-        # get integration
         expq_eps = np.exp(q_eps)
         def f(q, IE):
             expq = np.exp(q)
-            IV = IE + (expq - expq_eps)**2
+            A = expq - expq_eps
+            IV = IE + A*A
             xV = np.log( IV_eps + IV )
             xrho = _xrhoxV(xV)
             return expq * _dxrhoxV( xV ) * np.exp(xrho) / np.exp(xV)
+        
         IEs = IVs + 0.
-        IEt = IEs[-1]        
-        Ints = np.array([ integ.quad( 
-            (lambda q: f(q, IE)), 
-            q_eps, np.log(np.sqrt(IEt-IE)+expq_eps), limit=1000 )[0] 
-            for IE in IEs ])
+        IEt = IVs[-1]
+        Ints = []
+        for i, IE in enumerate(IEs):
+            f2int = lambda q: f(q, IE)
+            q_lo, q_hi = q_eps, np.log(np.sqrt(IEt-IE)+expq_eps)
+            Ints.append( self['integ']( f2int, q_lo, q_hi, 
+                epsrel=1.0e-4, limit=500 )[0] )
+        Ints = np.array(Ints)
         
         # make derivative again
         xEs = np.log( IE_eps + IEs ) 
         xInts = np.log( Int_eps - Ints )
-        _xIntxE = self.interp( xEs, xInts )
-        _dxIntdxE = _xIntxE.derivative()
-        #dxIntdxE = _xIntxE.derivative()( xEs )
-        #for i in range(len(xEs)-1):
-        #    if dxIntdxE[i+1] < dxIntdxE[i]: dxIntdxE[i+1] = dxIntdxE[i]
-        #_dxIntdxE = self.interp( xEs, dxIntdxE )
-        self._data.update(dict(xEs=xEs,xInts=xInts))
-        
-        print('interp for fE done, xE = [%f...%f], xInt = [%f...%f]'%( \
-            xEs[0], xEs[-1], xInts[0], xInts[-1] ))
+        self._tags.update({'xEs':xEs,'xInts':xInts,'IEs':IEs})
+        _xIntxE = self['interp']( xEs, xInts )
+        _dxIntdxE = mystats.InterpBound(xEs[0], xEs[-1], _xIntxE.derivative())
+        _xIntxE = mystats.InterpBound(xEs[0], xEs[-1], _xIntxE)
         def _IfxE(xE):
             xInt = _xIntxE(xE)
             dxIntdxE = _dxIntdxE(xE)
             return - np.exp(xInt) / np.exp(xE) * dxIntdxE
-        self._IfxE = _IfxE
-        self.fE0 = 2.0/( np.sqrt(8.0)*np.pi*np.pi ) * self.rho0 \
-            / ( 3.0*self.Vs )**1.5
+        #self._IfxE = mystats.InterpBound(xEs[0], xEs[-400], _IfxE) 
+        self._IfxE = mystats.InterpBound(xEs[0], xEs[-1], _IfxE) 
+        self._tags['fE0'] = 2.0/( np.sqrt(8.0)*np.pi*np.pi ) * self['rho0'] \
+            / ( 3.0*self['Vs'] )**1.5
+        print('  Interp for fE done, xE = [%.4g...%.4g], xInt = [%.4g...%.4g]'%( \
+            xEs[0], xEs[-1], xInts[0], xInts[-1] ))
+
+
+class NFWProfile(HaloProfileBase):
+    def __init__(self, mass, rs, rvir=None, rt=None, rw=None, 
+        r0=None, rcore=None, units=None, name='NFW' ):
+        self._tags = {'mass': mass, 'name': name}
+        
+        _rs = rs
+        _rvir = rvir if rvir is not None else 15.0*_rs
+        _rt = rt if rt is not None else 2.0*_rvir
+        _rw = rw if rw is not None else 0.2*_rvir
+        _r0 = r0 if r0 is not None else 1.0e-4*_rs
+        _rin = 0.1*_r0
+        _rcore = rcore if rcore is not None else 1.0e-10*_rs
+        r_vals = np.array([_rin, _r0, _rcore, 
+            _rs, _rvir, _rw, _rt],dtype=float)
+        
+        for ir, rpost in enumerate(('in', '0', 'core', 's', 'vir', 'w', 't')):
+            r_val = r_vals[ir]
+            self._tags.update({ 
+                'r'+rpost: r_val, 'x'+rpost: r_val/_rs, 
+                'y'+rpost: np.log(r_val/_rs) })
+        
+        self._tags['units'] = units if units else Units()
+        y_ins = np.linspace(self['y0'], np.log( 0.8*self['xt'] ), 1500 )
+        y_outs = np.log( np.linspace(0.801*self['xt'], self['xt'], 400) )
+        self._tags['y_vals'] = np.hstack( (y_ins, y_outs) )
+        HaloProfileBase.__init__(self)
+    def _Irho(self, x):
+        xp1 = 1.0+x
+        xpc = self['xcore']+x
+        return 1.0 / ( xpc*xp1*xp1 ) * \
+            scipy.special.erf( (self['xt']-x)/self['xw'] )
+    
+class HernquistProfile(NFWProfile):
+    def __init__(self, mass, rs, rvir=None, rt=None, rw=None, 
+        r0=None, rcore=None, units=None, name='Hernquist' ):
+        NFWProfile.__init__(self, mass, rs, rvir, rt, rw, 
+            r0, rcore, units, name)
+    def _Irho(self, x):
+        xp1 = 1.0+x
+        xpc = self['xcore']+x
+        return 1.0/( xpc*xp1*xp1*xp1 ) * \
+            scipy.special.erf( (self['xt']-x)/self['xw'] )
